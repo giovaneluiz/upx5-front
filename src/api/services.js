1d219be60c33f611ff65f59d33271ad2f28f0997
giovaneluiz <giovaneluiz19@yahoo.com.br>
2024-06-09 18:50:13 -0300

    iniciando serviço

--- src/api/services.js
+++ src/api/services.js
@@ -1,9 +1,13 @@
 import axios from "axios"
-import { env } from '../../env'
+import { env } from "../../env"
 
 export const getUserAll = async () => {
-  try {
-    const res = await axios.get(`${env.BASEURL}/user`)
+  try {      
+    const res = await axios.get(`${env.BASEURL}/user`, {
+      headers: {
+        "Content-Type": 'application/json'
+      }
+    })        
     if (res) {
       return res.status === 200 ? res.data : null
     }
