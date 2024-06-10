a6ac584180b144f01dd48d0660c46ffdc42d1a39
giovaneluiz <giovaneluiz19@yahoo.com.br>
2024-06-10 09:08:31 -0300

    tentativa de modificação do User-Agent

--- src/api/services.js
+++ src/api/services.js
@@ -1,11 +1,11 @@
-import axios from "axios"
-import { env } from "../../env"
+import axios from 'axios'
+import { env } from '../../env'
 
 export const getUserAll = async () => {
   try {      
-    const res = await axios.get(`${env.BASEURL}/user`, {
+    const res = await axios.get(`${env.BASE_URL_UPX5}/user`, {
       headers: {
-        "Content-Type": 'application/json'
+      'Content-Type': 'application/json'
       }
     })        
     if (res) {
